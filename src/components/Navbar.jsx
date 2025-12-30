522dbab64c76bf139e6707d30cbf11684152b4d4
fcaigdc <fcaigamedevclub@gmail.com>
2025-12-30 17:37:09 +0200

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import logoImageFile from '/omar.webp'; // عدل المسار حسب ملفك
-import './styles/navbar.css'; // استورد الـ CSS العام
+import './src/styles/navbar.css'; // استورد الـ CSS العام
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
